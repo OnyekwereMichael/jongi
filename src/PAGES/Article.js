98343cb2d9d4ed8a179d1a94499f89a72e27031a
Onyekwere Michael <onyekweremichael55@gmail.com>
2024-01-15 11:15:06 -0800

    jongi

--- src/PAGES/Article.js
+++ src/PAGES/Article.js
@@ -1,68 +1,64 @@
-import React from 'react';
-import RECT from '../Assests/Rectangle 1 (1).png'
-import red from '../Assests/_.png'
-import { Link } from 'react-router-dom';
-import {motion } from "framer-motion"
-
+import React from "react";
+import RECT from "../Assests/Rectangle 1 (1).png";
+import red from "../Assests/_.png";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Article() {
   return (
-                <motion.main
-                 >
-              <section className='main_sec'>
-            <div>
-                <p >CREATIVE AGENCY</p>
-            </div>
-            <div>
-                <p>WHAT CAN WE DO FOR YOU?</p>
-            </div>
-            </section>
-
-          
-            <section>
-                <img src={RECT} alt="" />
-          </section>
-
-
-            <section>
-              <p className='art_sec'>{"Dedicated to exceeding client's expectation."}</p>
-          </section>
+    <motion.main>
+      <section className="main_sec">
+        <div>
+          <p>CREATIVE AGENCY</p>
+        </div>
+        <div>
+          <p>WHAT CAN WE DO FOR YOU?</p>
+        </div>
+      </section>
 
       <section>
-            <p className='agency'>We are a creative agency dedicated to exceeding client's expectation seeting benchmarks in the industry, transfroming ideas into extra ordinary experience that captivate audiences and prospels businessess towards sucess</p>
+        <img src={RECT} alt="" />
       </section>
 
+      <section className="creative">
+        <section>
+          <p className="art_sec">
+            <span>{"Dedicated to exceeding client's expectation."}</span>
+          </p>
 
-    <article className='double'>
-      <section className='About'>
-        <div>
+          <p className="agency">
+            <span>
+              We are a creative agency dedicated to exceeding client's
+              expectation seeting benchmarks in the industry, transfroming ideas
+              into extra ordinary experience that captivate audiences and
+              prospels businessess towards sucess
+            </span>
+          </p>
+        </section>
+      </section>
+
+      <article className="double">
+        <section className="About">
+          <div>
             <a href="">ABOUT US</a>
-        </div>
-        <div>
-            <img src={red} alt="" className='ARROW'/>
-        </div>
-      </section>
+          </div>
+          <div>
+            <img src={red} alt="" className="ARROW" />
+          </div>
+        </section>
 
-      <section className='About'>
-        <div>
+        <section className="About">
+          <div>
             <a href="">WHAT WE DO</a>
-        </div>
+          </div>
 
-        <div>
-            <img src={red} alt="" className='ARROW'/>
-        </div>
-        
-      </section>
+          <div>
+            <img src={red} alt="" className="ARROW" />
+          </div>
+        </section>
       </article>
-            </motion.main>
-    
-
-  )
+    </motion.main>
+  );
 }
 
-
 export default Article;
-
-
-
-
